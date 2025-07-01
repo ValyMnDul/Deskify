ca25d5a4bcb9b7fb99616b7ef2c3c22c159848c6
ValyMnD <mindrilavasilevalentin@gmail.com>
2025-07-01 00:17:23 -0700

    menu

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,6 +1,9 @@
 import './Topbar.css';
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 export default function Topbar(){
+
+    const dayOption=useRef<HTMLDivElement|null>(null);
+    const [dayOptionValue,setDayOptionValue]=useState(true);
 
     const format0 = (num:number) => num.toString().padStart(2, '0');
 
@@ -35,11 +38,58 @@ export default function Topbar(){
         setTop((p)=>(!p));
     };
 
+    const optionPressed=(input:string)=>{
+       switch(input){
+            case 'day':
+                if(dayOption.current) {
+                    if(!dayOptionValue){
+                        dayOption.current.style.justifyContent="flex-end";
+                        dayOption.current.style.backgroundColor="rgb(176, 98, 98)";
+                    }
+                    else{
+                        dayOption.current.style.justifyContent="flex-start";
+                        dayOption.current.style.backgroundColor="aliceblue";
+                    }
+                }
+                setDayOptionValue((p)=>(!p));
+       }
+    };
+
     return(
         <div className='topbar'>
-            <div className='clock' onClick={clock}>
-                <p>{format0(month)}/{format0(day)}/{year} &nbsp; {format0(hour)}:{format0(minute)}:{format0(second)}</p>
-                {top ? <div className='topbarMenu'></div> : null}
+            <div className='clock'>
+                <p onClick={clock}>{format0(month)}/{format0(day)}/{year} &nbsp; {format0(hour)}:{format0(minute)}:{format0(second)}</p>
+                {top ? <div className='topbarMenu'>
+                    <div>
+                        <div className='topbarOption'>
+                            <div ref={dayOption} onClick={()=>{optionPressed('day')}}><div></div></div>
+                            <p>Day</p>
+                        </div>
+                        <div className='topbarOption'>
+                            <div><div></div></div>
+                            <p>Month</p>
+                        </div>
+                        <div className='topbarOption'>
+                            <div><div></div></div>
+                            <p>Year</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className='topbarOption'>
+                            <div><div></div></div>
+                            <p>Second</p>
+                        </div>
+                        <div className='topbarOption'>
+                            <div><div></div></div>
+                            <p>Minute</p>
+                        </div>
+                        <div className='topbarOption'>
+                            <div><div></div></div>
+                            <p>Hour</p>
+                        </div>
+
+                    </div>
+                </div> : null}
             </div>
         </div>
     );
